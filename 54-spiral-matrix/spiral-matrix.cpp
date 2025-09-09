18b33fd8f7b765f40e2c0fd18b2d8bfa6bfb1318
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-09-09 19:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (49.56%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int srow=0,erow=n-1,scol=0,ecol=m-1;
         vector<int>result;
-        int n =matrix.size();
-        int m= matrix[0].size();
-        int srow=0,scol=0,erow=n-1,ecol=m-1;
-       while(srow<=erow && scol<=ecol){
-        //top
-        for(int i=srow;i<=ecol;i++){
+        while(srow<=erow && scol<=ecol){
+       //Up
+       for(int i =srow;i<=ecol;i++){
             result.push_back(matrix[srow][i]);
        }
        //Right
        for(int i=srow+1;i<=erow;i++){
-           result.push_back(matrix[i][ecol]);
-    }   
-       //bottom
+        result.push_back(matrix[i][ecol]);
+       }
+       //Down
        for(int i=ecol-1;i>=scol;i--){
         if(srow==erow) break;
         result.push_back(matrix[erow][i]);
        }
-
-       //left
+       //Left
        for(int i=erow-1;i>srow;i--){
         if(scol==ecol) break;
         result.push_back(matrix[i][scol]);
        }
-           srow++,scol++,ecol--,erow--;
-       }
-       return result;
+       srow++,erow--,scol++,ecol--;
+        }
+        return result;
     }
 };
