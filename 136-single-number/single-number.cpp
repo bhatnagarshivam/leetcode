ab67aacd6ee18ae49db4ea2015ddf76960ab3bb0
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-09-21 22:04:43 +0530

    Time: 12 ms (13.10%) | Memory: 20.7 MB (58.09%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        map<int,int>freq;
-        for(auto i :nums){
-            freq[i]++;
-        }
-        for(auto c : freq){
-            if(c.second==1){
-                return c.first;
+        
+        sort(nums.begin(),nums.end());
+        for(int i= 0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                i++;
+            }
+            else{
+                return nums[i];
             }
         }
-        return 0;
+        return nums[n-1];
     }
 };
