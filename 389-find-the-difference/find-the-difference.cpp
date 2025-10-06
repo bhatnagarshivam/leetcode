fc916b4dbe2323e277dab5805128db41d3683577
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-10-06 18:54:44 +0530

    Time: 2 ms (19.76%) | Memory: 9.2 MB (52.11%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int n = s.size();
-        int m = t.size();
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<m-1;i++){
-            if(s[i]!=t[i]){
-                return t[i];
-            }
+       sort(s.begin(),s.end());
+       sort(t.begin(),t.end());
+       for(int i=0;i<n;i++){
+        if(s[i]!=t[i]){
+            return t[i];
         }
-        return t[n];
+       }
+       return t[n];
     }
 };
