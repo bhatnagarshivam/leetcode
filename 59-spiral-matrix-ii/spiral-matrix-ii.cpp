fe7a55e19ab59b5b3b3d86a8ec47dac9ed370a45
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-10-07 20:50:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (38.77%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>result(n,vector<int>(n,0));
-        int count=1;
-        int srow=0,scol=0,erow=n-1,ecol=n-1;
-        while(srow<=erow && scol<=ecol){
-            //Up
-            for(int i=srow;i<=ecol;i++){
-                result[srow][i]=count++;
+      vector<vector<int>>result(n,vector<int>(n));
+      int srow=0,scol=0,erow=n-1,ecol=n-1;
+      int k=1;
+        while(srow <= erow  && scol <= ecol){
+
+            for(int i=scol;i<=ecol;i++){
+                result[srow][i]=k++;
             }
-            //Right
-            for(int i=srow+1;i<=erow;i++){
-                result[i][ecol]=count++;
-            }
-            //down
-            for(int i=ecol-1;i>=scol;i--){
-                if(srow==erow) break;
-                result[erow][i]=count++;
-            }
-            // left
-            for(int i=erow-1;i>srow;i--){
-                if(scol==ecol) break;
-                result[i][srow]=count++;
-            }
-            srow++,scol++,erow--,ecol--;
-        }
-        
-        return result;
+             for(int i=srow+1;i<=erow;i++){
+                result[i][ecol]=k++;
+             }
+             for(int i=ecol-1;i>=scol;i--){
+                if(srow==erow)  break;
+               result[erow][i]=k++;
+            } 
+
+             for(int i=erow-1;i>srow;i--){
+                if(scol==ecol)  break;
+               result[i][scol]=k++;
+             }
+            srow++,erow--,scol++,ecol--;
+             }
+        return result;     
     }
 };
