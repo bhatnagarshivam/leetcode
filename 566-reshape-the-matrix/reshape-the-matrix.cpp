5463d25b6cf4860aa219503d578653ceb653b1ae
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-10-11 00:12:16 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (23.19%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        if(r*c!=mat.size()*mat[0].size()){
+            return mat;
+        }
+       vector<vector<int>>result(r,vector<int>(c));
+       int row=0;
+       int column=0;
+       for(int i=0;i<mat.size();i++){
+        for(int j=0;j<mat[0].size();j++){
+            if(column>=c){
+                row++;
+                column=0;
+            }
+            result[row][column++]=mat[i][j];
+        }
+       }
+       return result;
+    }
+};
