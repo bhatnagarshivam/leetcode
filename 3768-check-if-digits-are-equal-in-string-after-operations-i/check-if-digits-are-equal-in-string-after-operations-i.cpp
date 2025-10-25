afb62d9fa98d04e3d844438328bb944ecd49004b
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-10-25 19:06:51 +0530

    Time: 4 ms (58.83%) | Memory: 10 MB (47.03%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool hasSameDigits(string s) {
-        if(s.size()<2)  return false;
-        while(s.size()>2){
-            string result="";
-            for(int i=1;i<s.size();i++){        
-                result+=((s[i]-'0')+(s[i-1]-'0'))%10;
+        while(s.length()>2){
+            string res="";
+            for(int i=1;i<s.length();i++){
+                res+=((s[i]-'0')+s[i-1]-'0')%10;
             }
-            s=result;
+            s=res;
         }
         return s[0]==s[1];
     }
