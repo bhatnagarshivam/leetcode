28e0af7f5fafd8254ad6e99a265aaf26c920ee70
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-10-28 22:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (50.00%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.cpp
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int secondHighest(string s) {
-        int firstlargest=-1,secondlargest=-1;
-        for(char c:s){
-            if(isdigit(c)){
-                int i=c-'0';
-                 if(i>firstlargest){
-                    secondlargest=firstlargest;
-                    firstlargest=i;
-                 }
-                 else if(i<firstlargest&&i>secondlargest){
-                    secondlargest=i;
-                 }
+     int n = s.size();
+     int firstlargest=-1,secondlargest=-1;
+     for(int i=0;i<n;i++){
+        if(isdigit(s[i])){
+            int val=s[i]-'0';
+            if(val>firstlargest){
+                  secondlargest=firstlargest;
+                   firstlargest=val;
+            }
+            else if(val<firstlargest && val>secondlargest){
+                secondlargest=val;
             }
         }
-        return secondlargest;
+     }
+      return secondlargest;
     }
 };
