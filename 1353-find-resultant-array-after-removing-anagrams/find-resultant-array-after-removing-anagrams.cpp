a50b8c7d13b21495ccc634805addd77d93ea18cc
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-10-29 17:34:25 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (91.45%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-      int n = words.size();
-      vector<string>result;
-      result.push_back(words[0]);
-      for(int i=1;i<n;i++){
-        string prev=result.back();
-        string current=words[i];
-         string sortprev=prev;
-         string sortcurrent=current;
-         sort(sortprev.begin(),sortprev.end());
-         sort(sortcurrent.begin(),sortcurrent.end());
-         if(sortprev!=sortcurrent){
-            result.push_back(current);
-         }
-      }
-      return result;
+        int n = words.size();
+        vector<string>result;
+        result.push_back(words[0]);
+        for(int i=1;i<n;i++){
+            string prev=result.back();
+            string current=words[i];
+            string prevsort=prev;
+            string currsort=current;
+            sort(prevsort.begin(),prevsort.end());
+            sort(currsort.begin(),currsort.end());
+            if(prevsort!=currsort) result.push_back(current);
+        }
+        return result;
     }
 };
