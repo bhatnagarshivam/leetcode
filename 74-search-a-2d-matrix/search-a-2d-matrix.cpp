096be782e32f8560bc52158672459a71c28e828e
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-11-08 00:09:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (75.64%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int top=0;
-        int bottom=n-1;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low=0;
+        int high=m-1;
         int row=-1;
-        while(top<=bottom){
-            int mid = (top+bottom)/2;
+        while(low<=high){
+            int mid = low + (high - low) / 2;
             if(matrix[mid][0]==target){
-                return true;
-            }
-            if(matrix[mid][0]<target){
-                row=mid;
-                top=mid+1;
-            }
+                return true;}
+            else if(matrix[mid][0]>target){ 
+                high=mid-1;}
             else{
-                bottom=mid-1;
+                    row=mid;
+                    low=mid+1;
             }
         }
         if(row==-1)  return false;
         int left=0;
-        int Right=m-1;
+        int Right=n-1;
         while(left<=Right){
-            int mid=(left+Right)/2;
-            if(matrix[row][mid]==target){
-                return true;
-            }
-            if(matrix[row][mid]<target){
-                left=mid+1;
-            }
-            else{
-                Right=mid-1;
-            }
+            int mid_row = left + (Right-left) /2 ;
+            if(matrix[row][mid_row]==target) return true;
+            else if (matrix[row][mid_row]>target) Right=mid_row-1;
+            else left=mid_row+1;
         }
         return false;
-        
     }
 };
