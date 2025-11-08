acfc2b06eeb0e9d54d0d7effda668ec26b739435
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-11-08 18:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (51.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -8,15 +8,15 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    row[i]=true;
-                    column[j]=true;
-                } 
+                   row[i]=true;
+                   column[j]=true;
+                }
             }
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(row[i]||column[j]){
-                  matrix[i][j]=0;
+                    matrix[i][j]=0;
                 }
             }
         }
