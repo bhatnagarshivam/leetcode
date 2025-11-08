0361a95f9bff24791a43ae1d2368b1397f3b692a
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-11-08 19:00:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (90.16%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-       int n = s.size();
-       int k=0;
-       int count=0;
-       for(int i=n-1;i>=0;i--){
-        if(s[i]==' ' && k==0) 
-        {continue;
+        int n = s.size();
+        int k=0,count=0;
+        for(int i=n-1;i>=0;i--){
+            if((s[i]==' ')&& k==0){
+                continue;
+            }
+            else if((s[i]==' ')&& k==1)
+            {
+                return count;
+            }
+            else{
+                k=1;
+                count++;
+            }
         }
-        else if(s[i]==' ' && k==1) 
-        {
-            return count;
-        }
-        else{
-            k=1;
-            count++;
-        }
-       }
-       return count;
+        return count;
     }
 };
