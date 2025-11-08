2ebc7b22e1482303136b6c66f7f6c04e03ccc0b0
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-11-08 18:13:08 +0530

    Time: 20 ms (52.66%) | Memory: 15.7 MB (74.85%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     string sortVowels(string s) {
-    int n = s.size();
-    string temp="";
-    for(int i=0;i<n;i++){
-        if(isvowel(s[i])){
-            temp+=s[i];
+        int n = s.size();
+        string vowel="";
+        for(int i=0;i<n;i++){
+            if(isvowel(s[i])){
+                vowel+=s[i];
+            }
         }
-    }
-    sort(temp.begin(),temp.end());
         int k=0;
-    for(int i=0;i<n;i++){
-        if(isvowel(s[i])){
-        s[i]=temp[k++];
+        sort(vowel.begin(),vowel.end());  
+        for(int i=0;i<n;i++){
+            if(isvowel(s[i])){
+              s[i]=vowel[k++];
+            }
+        }
+        return s;
     }
-   } 
-   return s;
-     } 
-     bool isvowel(char c){
-         c=tolower(c);
-          return (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')?true:false;
-       
-     }    
+    bool isvowel(char c){
+        char temp = tolower(c);
+        if(temp=='a'||temp=='e'||temp=='i'||temp=='o'||temp=='u'){
+            return true;
+        }
+        return false;
+    }
 };
