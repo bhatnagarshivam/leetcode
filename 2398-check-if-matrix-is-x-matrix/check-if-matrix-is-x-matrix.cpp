5d933306b55db5f6e53ee69559e7af3936d0c295
bhatnagarshivam <shivambhatnagar124@gmail.com>
2025-11-17 23:36:53 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (54.07%) - LeetSync

--- 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.cpp
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.cpp
@@ -5,10 +5,15 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i==j || i+j==n-1){
-                if(grid[i][j]==0) return false;
+                    if(grid[i][j]==0){
+                        return false;
+                    }
                 }
                 else{
-                if(grid[i][j]!=0) return false;
+                   if(grid[i][j]!=0){
+                    return false;
+                   }
+                     
                 }
             }
         }
