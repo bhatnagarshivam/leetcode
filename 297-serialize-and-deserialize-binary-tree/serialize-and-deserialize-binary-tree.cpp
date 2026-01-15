5e48e346bfd11f006c85a13968170e380b0d5758
bhatnagarshivam <shivambhatnagar124@gmail.com>
2026-01-15 22:22:38 +0530

    Time: 33 ms (28.29%) | Memory: 32.3 MB (60.10%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -12,52 +12,51 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(root==NULL) return "";
-        string result="";
+         if(root==NULL) return "";
+         string serialize="";
+         queue<TreeNode*>q;
+         q.push(root);
+         while(!q.empty()){
+            TreeNode*node=q.front();
+            q.pop();
+            if(node!=NULL) {
+            serialize+=to_string(node->val)+",";
+             q.push(node->left);
+             q.push(node->right);
+            }
+             else {
+                serialize+="null,";}
+         }
+         return serialize;
+        
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data=="") return NULL;
+        stringstream ss(data);
+        string item;
+        getline(ss,item,',');
+        TreeNode*root=new TreeNode(stoi(item));
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
             TreeNode*node=q.front();
             q.pop();
-            if(node){
-             result+=to_string(node->val)+",";
-             q.push(node->left);
-             q.push(node->right);
-            }
-            else{
-                result+="null,";
-            }
-        }
-        return result;
-    }
-
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-           if(data=="") return NULL;
-           stringstream ss(data);
-           string item;
-           getline(ss,item,',');
-           TreeNode*root=new TreeNode(stoi(item));
-            queue<TreeNode*>q;
-            q.push(root);
-             while(!q.empty()){
-              TreeNode*node=q.front();
-              q.pop();
-             getline(ss,item,',');
-             //left root
-             if(item!="null"){
+            getline(ss,item,',');
+            if(item!="null"){
                 node->left=new TreeNode(stoi(item));
-                q.push(node->left);
-             }
-              getline(ss,item,',');
-             //Right root
-             if(item!="null"){
+                 q.push(node->left);
+            }
+            getline(ss,item,',');
+            if(item!="null"){
                 node->right=new TreeNode(stoi(item));
                 q.push(node->right);
-             }  
-             }  
-        return root;
-    }
+            }
+        }
+return root;
+
+        }
 };
 
 // Your Codec object will be instantiated and called as such:
