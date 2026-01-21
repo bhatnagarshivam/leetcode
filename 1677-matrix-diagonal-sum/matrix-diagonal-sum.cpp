3aba40bda513c75cd6c100cada44fabb62668230
bhatnagarshivam <shivambhatnagar124@gmail.com>
2026-01-21 23:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (52.19%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int result=0;
-        for(int i=0;i<n;i++){
+        int m= mat.size();
+        int n = mat[0].size();
+        int diagonalsum=0;
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==j || i+j==n-1){
-                    result+=mat[i][j];
+                    diagonalsum+=mat[i][j];
                 }
             }
         }
-    return result;
+        return diagonalsum;
     }
 };
