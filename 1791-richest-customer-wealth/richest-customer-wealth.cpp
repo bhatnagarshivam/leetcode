3bef7acb09621283a5e3216b73fece9ae492777b
bhatnagarshivam <shivambhatnagar124@gmail.com>
2026-01-21 23:25:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (12.47%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -3,14 +3,14 @@ public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int m = accounts.size();
         int n = accounts[0].size();
-        int MaxWealth=0;
-        for(int i=0;i<m;i++){
-            int CurrentMax=0;
+         int max_wealth=0,curr_wealth=0;
+         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-               CurrentMax+=accounts[i][j];
+                curr_wealth+=accounts[i][j];
             }
-            MaxWealth=max(MaxWealth,CurrentMax);
-        }
-    return MaxWealth; 
+            max_wealth=max(max_wealth,curr_wealth);
+            curr_wealth=0;
+         }
+         return max_wealth;
     }
 };
