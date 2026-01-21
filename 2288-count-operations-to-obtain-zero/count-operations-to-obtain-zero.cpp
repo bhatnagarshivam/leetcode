7655d56b7e9b009a36d015dbe6e84c8d7985808c
bhatnagarshivam <shivambhatnagar124@gmail.com>
2026-01-21 22:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.71%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        int operation=0;
-        while(num1 && num2){
-           if(num1>=num2) num1=num1-num2;
-           else num2=num2-num1;
-           operation++;
-        }
-         return operation;
-    } 
+        int count=0;
+    while(num1 !=0  && num2 !=0){
+         count++;
+        if(num1>=num2)
+         num1=num1-num2;
+        else 
+        num2=num2-num1;
+    }
+       return count; 
+    }
 };
