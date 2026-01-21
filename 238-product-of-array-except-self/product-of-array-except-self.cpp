c76e0758283d036aa1b9fb66ffc7a29fab7d7cf9
bhatnagarshivam <shivambhatnagar124@gmail.com>
2026-01-21 20:59:21 +0530

    Time: 4 ms (21.31%) | Memory: 45.3 MB (5.52%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int>prefixProd;
-        vector<int>suffixProd;
-        int prefixprod=1;
-        int suffixprod=1;
-        prefixProd.push_back(prefixprod);
-        for(int i=1;i<n;i++){
-             prefixprod*=nums[i-1];
-               prefixProd.push_back(prefixprod);
-        }
-         suffixProd.push_back(suffixprod);
-        for(int i=n-2;i>=0;i--){
-             suffixprod*=nums[i+1];
-               suffixProd.push_back(suffixprod);
-        }
-        reverse(suffixProd.begin(),suffixProd.end());
-        for(int i=0;i<n;i++){
-            prefixProd[i]=prefixProd[i]*suffixProd[i];
-        }
-        return prefixProd;
+    vector<int>prefix;
+    vector<int>suffix;
+    int prefixprod=1;
+    int  suffixprod=1;
+        prefix.push_back(1);
+    for(int i=1;i<n;i++){
+         prefixprod *=nums[i-1];
+        prefix.push_back(prefixprod);
+    }   
+    suffix.push_back(1);
+    for(int i =n-2;i>=0;i--){
+        suffixprod*=nums[i+1];
+        suffix.push_back(suffixprod);
+    }
+    reverse(suffix.begin(),suffix.end());
+     for(int i=0;i<n;i++){
+        nums[i]=suffix[i]*prefix[i];
+     }
+     return nums;
     }
 };
