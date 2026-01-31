675a6efa3863ebcc0cf192f5932e7cd18f6733e4
bhatnagarshivam <shivambhatnagar124@gmail.com>
2026-01-31 16:40:38 +0530

    Time: 4 ms (51.97%) | Memory: 118 MB (82.21%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,32 +11,29 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(head->next==NULL) return true;
-        ListNode*curr=head;
-        ListNode*dummy=head;
-        int count=0;
-        while(curr){
-            count++;
-            curr=curr->next;
+        if(head==NULL || head->next==NULL) return true;
+        ListNode*slow=head;
+        ListNode*fast=head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int mid=count/2;
-        while(--mid){
-            dummy=dummy->next;
+        ListNode*curr=slow->next;
+        slow->next=NULL;
+        ListNode*prev=NULL;
+        while(curr!=NULL){
+            ListNode*temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
         }
-         ListNode*rev=dummy->next;
-         dummy->next=NULL;
-         ListNode*prev=NULL;
-         while(rev){
-            ListNode*temp=rev->next;
-             rev->next=prev;
-             prev=rev;
-             rev=temp;
-         }
-         while(head && prev){
-            if(head->val!=prev->val) return false;
+       while(head && prev){
+          if(head->val!=prev->val){
+            return false;
+          }
             head=head->next;
             prev=prev->next;
-         }
-         return true;
+       }
+        return true;
     }
 };
