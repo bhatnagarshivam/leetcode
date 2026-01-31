f9ce6de7302042931f7256ccea95af335ad11940
bhatnagarshivam <shivambhatnagar124@gmail.com>
2026-01-31 22:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (61.55%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,23 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode*dummy=new ListNode(-1);
+        if(list1==NULL && list2==NULL) return NULL;
+        if(list1!=NULL && list2==NULL) return list1;
+        else if(list1==NULL && list2!=NULL) return list2;
+        ListNode*dummy = new ListNode(-1);
         ListNode*ans=dummy;
         ListNode*ptr1=list1;
         ListNode*ptr2=list2;
-        while(ptr1 && ptr2){
+        while(ptr1!=NULL && ptr2!=NULL){
             if(ptr1->val <= ptr2->val){
-                dummy->next= ptr1;
+                dummy->next=ptr1;
+                dummy=dummy->next;
                 ptr1=ptr1->next;
             }
-            else if(ptr1->val>ptr2->val){
+            else if(ptr1->val > ptr2->val){
                 dummy->next=ptr2;
+                dummy=dummy->next;
                 ptr2=ptr2->next;
             }
-            dummy=dummy->next;
-            }
-            if(ptr1!=NULL) dummy->next=ptr1;
-            if(ptr2!=NULL) dummy->next=ptr2;
-            return ans->next; 
+        }
+        if(ptr1) dummy->next=ptr1;
+        else if(ptr2) dummy->next=ptr2;
+        return ans->next;
     }
 };
