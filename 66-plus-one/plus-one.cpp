ca97a2db3e67d69c07ff6cb13cc3512e114a86db
bhatnagarshivam <shivambhatnagar124@gmail.com>
2026-02-02 23:53:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (52.20%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -4,13 +4,13 @@ public:
         int n = digits.size();
         for(int i=n-1;i>=0;i--){
             if(digits[i]<9){
-                digits[i]++;
+                digits[i]+=1;
                 return digits;
             }
             digits[i]=0;
-        
         }
         digits.insert(digits.begin(),1);
         return digits;
+
     }
 };
